b8168a66a88177010d068568c17d2bbf789a58de
Maksym-Bohun <maks.boggun@gmail.com>
2023-09-10 16:52:47 +0300

    changed background color and text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <main className="main">ТРСПО</main>
+      <main className="main">
+        <div>ТРСПО</div>
+        <div>Лабораторна робота 1</div>
+      </main>
     </div>
   );
 }
